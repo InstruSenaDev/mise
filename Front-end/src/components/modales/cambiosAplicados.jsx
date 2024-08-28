5f6e2381b8311cc5426ce0fe27de6a7dd0b4debb
Juanfer2102 <160734369+Aleej0@users.noreply.github.com>
2024-08-28 07:25:44 -0500

    a

--- Front-end/src/components/modales/cambiosAplicados.jsx
+++ Front-end/src/components/modales/cambiosAplicados.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import VerifyIcon from "../../images/icons/iconsModales/verifyIcon.svg"
 
 export const CambiosAplicados = () => {
   return (
-    <div className='bg-white w-[40rem] justify-between flex '>
-        <div className="bg-grey flex-1">
-            <div></div>
-        </div>
-        <div className="flex-">
-            <p>¡Aplicado correctamente!</p>
-            <p>Se ha aplicado correctamente los cambios.</p>
+    <div className='bg-white w-[40rem] h-[5rem] justify-between flex rounded-2xl'>
+        <div className="bg-grey rounded-l-2xl flex items-center">
+            <img src={VerifyIcon.src} className='text-principalGreen' alt="" />
         </div>
         <div className="flex-1">
+            <p>¡Aplicado correctamente!</p>
+            <p>Se ha creado el usuario.</p>
+        </div>
+        <div className="flex-auto">
             <div className='bg-principalGreen text-black'>
                 <p>x</p>
             </div>
